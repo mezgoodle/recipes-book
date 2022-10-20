4f69a891e8e2839116e1ae98c8c2dfa2460c872f
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-10-20 13:20:48 +0300

    Show users table

--- recipes/dashboard/views.py
+++ recipes/dashboard/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from .models import TelegramUser
+
 def index(request):
-    return render(request, 'dashboard/index.html')
+    users = TelegramUser.objects.all().order_by('-id')
+    return render(request, 'dashboard/index.html', {'users': users})
+
+
+def recipes(request):
+    return render(request, 'dashboard/recipes.html')
