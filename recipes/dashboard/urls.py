dae02ce7e41d79c0dfce43adba8e024639c532ee
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-10-20 15:32:11 +0300

    Write delete logic

--- recipes/dashboard/urls.py
+++ recipes/dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('recipes', views.recipes, name='recipes'),
+    path('recipes/delete/<int:recipe_id>', views.delete_recipe, name='delete_recipe'),
 ]
--- recipes/dashboard/views.py
+++ recipes/dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import TelegramUser, Recipe
 
@@ -10,3 +10,8 @@ def index(request):
 def recipes(request):
     recipes = Recipe.objects.all().order_by('-id')
     return render(request, 'dashboard/recipes.html', {'recipes': recipes})
+
+
+def delete_recipe(request, recipe_id):
+    Recipe.objects.filter(id=recipe_id).delete()
+    return redirect('recipes')
