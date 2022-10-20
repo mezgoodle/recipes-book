a1183768e4f536b808d10c52b9b2e0257d1aa7cb
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-10-20 13:20:35 +0300

    Add field to the model

--- recipes/dashboard/models.py
+++ recipes/dashboard/models.py
@@ -4,6 +4,7 @@ class TelegramUser(models.Model):
     name = models.CharField(max_length=100, verbose_name='Ім\'я користувача в телеграмі')
     surname = models.CharField(max_length=100, verbose_name='Прізвище користувача в телеграмі')
     username = models.CharField(max_length=100, verbose_name='Юзернейм користувача в телеграмі')
+    telegram_id = models.CharField(max_length=100, verbose_name='Айді користувача в телеграмі')
     questioned_name = models.CharField(max_length=100, verbose_name='Ім\'я користувача з анкети')
     sex = models.CharField(max_length=100, verbose_name='Стать користувача в телеграмі') # TODO: maybe do as choice field
 
@@ -12,4 +13,4 @@ class TelegramUser(models.Model):
         verbose_name_plural = 'Користувачі'
 
     def __str__(self):
-        return f'#{self.id} @{self.username}'
+        return f'@{self.username}'
