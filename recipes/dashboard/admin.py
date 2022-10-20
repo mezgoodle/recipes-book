d8cef6ca37f91fd3d50c17b776244fa01acdb4c9
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-10-20 14:55:09 +0300

    Create Recipe model

--- recipes/dashboard/admin.py
+++ recipes/dashboard/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import TelegramUser
+from .models import TelegramUser, Recipe
 
 @admin.register(TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
     pass
--- recipes/dashboard/models.py
+++ recipes/dashboard/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 class TelegramUser(models.Model):
@@ -14,3 +15,16 @@ class TelegramUser(models.Model):
 
     def __str__(self):
         return f'@{self.username}'
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Назва рецепту')
+    description = models.CharField(max_length=100, verbose_name='Опис рецепту')
+    photo = models.ImageField(upload_to ='uploads/', verbose_name='Фото рецепту')
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепти'
+
+    def __str__(self):
+        return f'@{self.name}'
