9136db8aefe425ffdc78a39250c991a18db5ee12
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-10-21 16:06:27 +0300

    Implement methods to work with states

--- db.py
+++ db.py
@@ -15,10 +15,8 @@ class Database:
     
     def select_all_recipes(self) -> List[tuple]:
         self.cur.execute("SELECT * FROM dashboard_recipe")
-
         rows = self.cur.fetchall()
         result = []
-
         for row in rows:
             result.append(row)
         return result
@@ -28,14 +26,30 @@ class Database:
         recipe = self.cur.fetchall()
         return recipe[0]
         
-    def get_user_info(self, tg_id: str) -> tuple:
-        self.cur.execute("SELECT * FROM dashboard_telegramuser WHERE telegram_id=?", (tg_id,))
+    def get_user_info(self, telegram_id: str) -> tuple:
+        self.cur.execute("SELECT * FROM dashboard_telegramuser WHERE telegram_id=?", (telegram_id,))
         user = self.cur.fetchall()
         return user[0]
 
     def create_user(self, user) -> int:
-        sql = '''INSERT INTO telegramuser(name, surname, username, questioned_name, sex, telegram_id)
+        sql = '''INSERT INTO dashboard_telegramuser(name, surname, username, questioned_name, sex, telegram_id)
                 VALUES(?,?,?,?,?,?)'''
         self.cur.execute(sql, user)
         self.conn.commit()
         return self.cur.lastrowid
+
+    def get_user_state(self, telegram_id) -> int:
+        self.cur.execute("SELECT * FROM dashboard_state WHERE telegram_id=?", (telegram_id,))
+        user = self.cur.fetchall()
+        if user:
+            return user[0]
+        return []
+
+    def create_user_state(self, telegram_id, state_name) -> int:
+        self.cur.execute('INSERT INTO dashboard_state(telegram_id, state) VALUES(?,?)', (telegram_id, state_name))
+        self.conn.commit()
+        return self.cur.lastrowid
+
+    def change_user_state(self, telegram_id, state_name) -> int:
+        self.cur.execute('UPDATE dashboard_state SET state = ? WHERE telegram_id = ?', (state_name, telegram_id))
+        self.conn.commit()
