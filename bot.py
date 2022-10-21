1049a92cafeeb4aa2f54e3e913bcb0c475682cb7
Maksym Zavalniuk <mezgoodle@gmail.com>
2022-10-21 16:07:27 +0300

    Change bot logic

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import os
 
 from db import Database
-from markups import create_sex_markup, create_recipes_markup
+from states import State
 
 API_TOKEN = os.getenv('TOKEN', 'token')
 
@@ -12,23 +12,15 @@ db = Database()
 
 @bot.message_handler(commands=['start'])
 def start_questions(message):
+    state = State(bot, message.from_user.id)
+    state.first(message.from_user.id)
     return bot.reply_to(message, 'Введіть ваше ім\'я')
-
-
-@bot.message_handler(commands=['recipes'])
-def show_recipies(message):
-    recipes = db.select_all_recipes()
-    markup = create_recipes_markup(recipes)
-    return bot.reply_to(message, 'Оберіть рецепт', reply_markup=markup)
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    recipe = db.get_recipe_info(message.text)
-    photo = open('./recipes/media/' + recipe[3], 'rb')
-    return bot.send_photo(message.chat.id, photo, recipe[2])
-    # markup = create_sex_markup()
-    # bot.reply_to(message, 'Оберіть стать', reply_markup=markup)
+    state = State(bot, message.from_user.id)
+    state.set_data(message)
 
 
 bot.infinity_polling()
